0eb43948d2db0ef1b89aa069832b802e5b5bb87e
ray <zengyanren@foxmail.com>
2022-02-23 15:37:17 +0800

    feat: add department tree response

--- server/model/autocode/response/sys_department.go
+++ server/model/autocode/response/sys_department.go
@@ -4,5 +4,5 @@ import "github.com/flipped-aurora/gin-vue-admin/server/model/autocode"
 
 type DepartmentTreeResponse struct {
 	autocode.SysDepartment
-	
+	Children []DepartmentTreeResponse `json:"children"`
 }
