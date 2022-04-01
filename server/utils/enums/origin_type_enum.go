807b7ef8a783680bffc36944b79018110e5944c6
ray <zengyanren@foxmail.com>
2022-04-01 11:50:43 +0800

    feat: 增加用户来源以及来源编号字段

---
+++ server/utils/enums/origin_type_enum.go
@@ -0,0 +1,9 @@
+package utils
+
+type OriginTypeEnum uint
+
+const(
+	OriginTypeBuildIn OriginTypeEnum = 0x01 // 内部
+	OriginTypeAttendance OriginTypeEnum = 0x02 // 考勤
+	OriginTypeErp OriginTypeEnum = 0x02 // erp
+)
